09acea7055ceed265cfdb4f9cda9a31adb846a25
Justin Grote <JustinGrote@users.noreply.github.com>
2021-04-13 09:38:07 -0700

    Make RequiredModules noninteractive

--- Source/Press.tasks.ps1
+++ Source/Press.tasks.ps1
@@ -18,8 +18,7 @@ Enter-Build {
     $ProgressPreference = 'SilentlyContinue'
     if (Test-Path $RequiredModuleManifest) {
         $InstallRequiredModuleScript = Join-Path $RequireModuleScript.InstalledLocation 'Install-RequiredModule.ps1'
-        $PSDefaultParameterValues['Install-Module:Force'] = $true
-        $ImportedModules = . $InstallRequiredModuleScript -RequiredModulesFile $RequiredModuleManifest -Import -ErrorAction Stop -WarningAction SilentlyContinue
+        $ImportedModules = . $InstallRequiredModuleScript -RequiredModulesFile $RequiredModuleManifest -Import -ErrorAction Stop -WarningAction SilentlyContinue -Confirm:$false
         $SCRIPT:PressSetting = Get-PressSetting -ConfigPath $BuildRoot
         New-Item -ItemType Directory -Path $PressSetting.Build.OutDir -Force | Out-Null
     }
