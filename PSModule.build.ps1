1c3832ae096dc1b194c1f5922e9193990a4c7fc0
Justin Grote <JustinGrote@users.noreply.github.com>
2021-04-13 20:09:34 -0700

    📦 Make PowerCD Specific File Copies their own tasks

--- PSModule.build.ps1
+++ PSModule.build.ps1
@@ -3,31 +3,13 @@ Import-Module -Name $PSScriptRoot\Source\Press.psd1 -Force
 
 #TODO: Replace this with using the existing task and a settings variable
 
+Task CopyGitVersionAndTasks -After Press.CopyModuleFiles {
+    $IncludeFiles = @(
+        "$($PressSetting.General.SrcRootDir)\GitVersion.default.yml"
+        "$($PressSetting.General.SrcRootDir)\Press.tasks.ps1"
+    ) | Resolve-Path
 
-Task Press.CopyModuleFiles @{
-    Inputs  = { 
-        Get-ChildItem -File -Recurse $PressSetting.General.SrcRootDir
-        $SCRIPT:IncludeFiles = (
-            "$($PressSetting.General.SrcRootDir)\GitVersion.default.yml",
-            "$($PressSetting.General.SrcRootDir)\Press.tasks.ps1"
-            | Resolve-Path
-        )
-        $IncludeFiles
-    }
-    Outputs = { 
-        $buildItems = Get-ChildItem -File -Recurse $PressSetting.Build.ModuleOutDir
-        if ($buildItems) { $buildItems } else { 'EmptyBuildOutputFolder' } 
-    }
-    Jobs    = {
-        Remove-BuildItem $PressSetting.Build.ModuleOutDir
-
-        $copyResult = Copy-PressModuleFiles @commonParams `
-            -Destination $PressSetting.Build.ModuleOutDir `
-            -PSModuleManifest $PressSetting.BuildEnvironment.PSModuleManifest `
-            -Include $SCRIPT:IncludeFiles
-
-        $PressSetting.OutputModuleManifest = $copyResult.OutputModuleManifest
-    }
+    Copy-Item $includeFiles -Destination $($PressSetting.Build.ModuleOutDir)
 }
 
 Task CopyRequiredModules -After Press.CopyModuleFiles {
