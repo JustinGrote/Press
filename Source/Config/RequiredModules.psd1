db993c16b315aa89b1ac524db3238ea179f39754
Justin Grote <JustinGrote@users.noreply.github.com>
2021-05-27 18:39:24 -0700

    📦 Stub out Invoke-Build bootstrap

--- Source/Config/RequiredModules.psd1
+++ Source/Config/RequiredModules.psd1
@@ -1,6 +1,6 @@
 @{
     BuildHelpers        = '2.0.16'
-    Pester              = '5.1.1'
+    Pester              = '5.2.2'
     PowershellForGithub = '0.16.0'
     PowerConfig         = '0.1.3'
 }
--- build.ps1
+++ build.ps1
@@ -1,6 +1,3 @@
-#requires -version 5
-using namespace System.IO
-
 <#
 .SYNOPSIS
 Bootstraps Invoke-Build and starts it with supplied parameters.
@@ -11,57 +8,13 @@ If you already have Invoke-Build installed, just use Invoke-Build instead of thi
 Starts Invoke-Build with the default parameters
 #>
 
-$ErrorActionPreference = 'Stop'
-
-#Add TLS 1.2 to potential security protocols on Windows Powershell. This is now required for powershell gallery
-if ($PSEdition -eq 'Desktop') {
-    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor 'Tls12'
+#This script bootstraps Invoke-Build, using a local copy if available
+$BootStrapInvokeBuildPath = "$PSScriptRoot/Scripts/BootstrapInvokeBuild.ps1"
+[String]$BootStrapInvokeBuildContent = if (Test-Path $BootStrapInvokeBuildPath) {
+    Get-Content -Raw $BootStrapInvokeBuildPath
+} else {
+    Invoke-WebRequest -UseBasicParsing -Uri 'https://raw.githubusercontent.com/JustinGrote/Press/main/Scripts/BootstrapInvokeBuild.ps1'
 }
 
-function BootstrapModule {
-    param (
-        $ModuleSpecification,
-        $Path = (Join-Path ([Environment]::GetFolderPath('LocalApplicationData')) 'Press')
-    )
-    $vEnvDir = New-Item -ItemType Directory -Force -Path $Path
-
-    $env:PSModulePath = $vEnvDir,$env:PSModulePath -join [io.path]::PathSeparator
-
-    #This is done for performance. If the module is found loaded it won't try to search filesystem
-    $existingModule = (Get-Module -FullyQualifiedName $moduleSpecification -ErrorAction SilentlyContinue)
-    if (-not $existingModule) {
-        $existingModule = (Get-Module -ListAvailable -FullyQualifiedName $moduleSpecification -ErrorAction SilentlyContinue)
-    }
-
-    if ($existingModule) {
-        Write-Verbose "Module $($moduleSpecification.ModuleName) was detected. Skipping bootstrap."
-        return
-    }
-
-    $moduleParams = @{
-        Name = $moduleSpecification.ModuleName
-        MinimumVersion = $moduleSpecification.ModuleVersion
-        MaximumVersion = $moduleSpecification.MaximumVersion
-        Force = $true
-        ErrorAction = 'Stop'
-    }
-    Write-Verbose "$($ModuleSpecification.ModuleName) not found locally. Bootstrapping..."
-    Save-Module @moduleParams -Path $vEnvDir
-    Import-Module @moduleParams
-}
-
-BootstrapModule @{
-    ModuleName = 'InvokeBuild'
-    ModuleVersion = '5.5.7'
-    MaximumVersion = '5.99.99'
-}
-
-#Passthrough Invoke-Build
-Push-Location $PSScriptRoot
-try {
-    Invoke-Expression "Invoke-Build $($args -join ' ')"
-} catch {
-    throw $PSItem
-} finally {
-    Pop-Location
-}
+$BootStrapInvokeBuild = [ScriptBlock]::Create($BootStrapInvokeBuildContent)
+. $BootStrapInvokeBuild @args
